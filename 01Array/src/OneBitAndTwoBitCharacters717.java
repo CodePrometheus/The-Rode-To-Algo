0d6e82e62e54fcd31be417c721a971c818c0f76e
CodePrometheus <Code_Prince@outlook.com>
2021-02-23 13:48:20 +0800

    2.23-1

--- 01Array/src/OneBitAndTwoBitCharacters717.java
+++ 01Array/src/OneBitAndTwoBitCharacters717.java
@@ -26,9 +26,6 @@ import org.junit.Test;
  */
 public class OneBitAndTwoBitCharacters717 {
 
-    /**
-     *
-     */
     public boolean isOneBitCharacter(int[] bits) {
         int start = 0;
         while (start < bits.length - 1) {
@@ -42,11 +39,26 @@ public class OneBitAndTwoBitCharacters717 {
         return start == bits.length - 1;
     }
 
+    /**
+     * 只与最后一个元素0的前面的连续1的个数有关系。
+     * 如果 1 的个数为偶数个，那么最后一位是一比特字符，如果 1 的个数为奇数个，那么最后一位不是一比特字符。
+     * 贪心
+     */
+    public boolean isOneBitCharacter2(int[] bits) {
+        int i = bits.length - 2;
+        while (i >= 0 && bits[i] > 0) {
+            i--;
+        }
+        return (bits.length - i) % 2 == 0;
+    }
+
+
     @Test
     public void isOneBitCharacterTest() {
         int[] bits = {1, 1, 1, 0};
         int[] b2 = {1, 0, 0};
         System.out.println(isOneBitCharacter(bits));
         System.out.println(isOneBitCharacter(b2));
+        System.out.println(isOneBitCharacter2(b2));
     }
 }
